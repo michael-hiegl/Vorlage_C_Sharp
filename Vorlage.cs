b1dad8bfffe817e416bae48d2b6478f5b7513de1
michael-hiegl <100510019+michael-hiegl@users.noreply.github.com>
2022-10-13 20:00:13 +0200

    Add files via upload

---
+++ Vorlage.cs
@@ -0,0 +1,23 @@
+﻿//Imports######################################################
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//#############################################################
+
+//Classes######################################################
+//#############################################################
+namespace _2022_10_13_Call_by_Value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Code#############################################
+            //#################################################
+            Console.WriteLine("Press ENTER to exit...");
+            Console.ReadKey();
+        }
+    }
+}
